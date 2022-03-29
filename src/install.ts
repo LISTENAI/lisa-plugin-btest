c969cd99761ddd5ab44b823577f5771f86c33e1f
xychen <xychen@listenai.com>
2022-03-29 14:03:21 +0800

    修复更新插件时的 EEXIST 错误

--- src/install.ts
+++ src/install.ts
@@ -1,8 +1,9 @@
-import { ensureDir, symlink } from 'fs-extra';
+import { ensureDir, remove, symlink } from 'fs-extra';
 import { join } from 'path';
 import { FRAMEWORK_DIR, LISA_BTEST_HOME } from './const';
 
 (async () => {
   await ensureDir(LISA_BTEST_HOME);
+  await remove(join(LISA_BTEST_HOME, 'framework'));
   await symlink(FRAMEWORK_DIR, join(LISA_BTEST_HOME, 'framework'));
 })();
