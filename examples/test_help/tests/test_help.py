206310fb0ad5df4ca860276fe87955f600f873a3
xychen <xychen@listenai.com>
2022-04-07 15:11:25 +0800

    examples: 增加基于 shell 的 help 测试示例

---
+++ examples/test_help/tests/test_help.py
@@ -0,0 +1,20 @@
+import pytest
+from btest import device
+from btest.shell import *
+
+
+@pytest.fixture
+def connected_device():
+    devices = device.load_devices('device-map.yml')
+    assert len(devices) > 0
+    shell = shell_open(devices[0]['shell'])
+    yield (shell)
+    shell.close()
+
+
+def test_help(connected_device):
+    (shell) = connected_device
+    shell_exec(shell, 'help')
+    assert shell_match(shell, 'Available commands:',
+                       full_match=True) is not None
+    assert shell_match(shell, 'Prints the help message.') is not None
