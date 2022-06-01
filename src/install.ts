958b841d18b4e2c873bf38cb6b56c8e3a9782da5
jameswang <105700413+listenai-jw@users.noreply.github.com>
2022-06-01 19:35:42 +0800

    revert changes

--- src/install.ts
+++ src/install.ts
@@ -13,31 +13,10 @@ import download from "@xingrz/download2";
   await remove(join(LISA_BTEST_HOME, 'framework'));
   await symlink(FRAMEWORK_DIR, join(LISA_BTEST_HOME, 'framework'));
 
-  //trigger @binary/python-3.9 download
-  console.log('Downloading python3.9 binary...');
-  const PACKAGE = 'python-3.9';
-  const VERSION = '3.9.7';
-  const NAME = `${PACKAGE}-${VERSION}-${process.platform}_${process.arch}.tar.zst`;
-
-  const pyPluginPath = resolve(__dirname, 'node_modules', '@binary', 'python-3.9', 'binary');
-  const pyPluginUrl = `https://cdn.iflyos.cn/public/lisa-binary/${PACKAGE}/${NAME}`;
-
-  await remove(pyPluginPath);
-  await download(pyPluginUrl, pyPluginPath, {
-    extract: true
-  });
-  if (process.platform != 'win32') {
-    await symlink('python3', join(pyPluginPath, 'bin', 'python'));
-  }
-  //const pyPluginPath = resolve(__dirname, 'node_modules', '@binary', 'python-3.9');
-  /*await remove(resolve(pyPluginPath, 'binary'));
-  await exec('node', [
-      resolve(pyPluginPath, 'lib', 'install.js')
-  ]);*/
-
   //install python venv
   console.log('Preparing isolated python environment...');
   const exec = extendExec();
+  const pyPluginPath = resolve(__dirname, 'node_modules', '@binary', 'python-3.9', 'binary');
   await exec(join(pyPluginPath, 'bin', 'python'), [
     "-m",
     "venv",
@@ -47,7 +26,7 @@ import download from "@xingrz/download2";
 
   //install default requirements
   console.log('Install default packages...');
-  await exec(join(PYTHON_VENV_DIR, 'Scripts', 'pip'), [
+  await exec(join(PYTHON_VENV_DIR, 'bin', 'pip'), [
       'install',
       '-i',
       PIP_INDEX_URL,
