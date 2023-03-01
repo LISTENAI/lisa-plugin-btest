9540ef30392424412972504aa7788967d5532f66
jameswang <105700413+listenai-jw@users.noreply.github.com>
2023-03-01 12:15:40 +0800

    (add) add compatibility for v1 and v2 device-map

--- framework/python/btest/conftest.py
+++ framework/python/btest/conftest.py
@@ -1,29 +1,26 @@
 import pytest
-import os
 from btest import device
 from btest.shell import *
 from btest.utils import logfile
 
+
 @pytest.fixture
 def connected_device(request):
-    # request = request.param
-    # dm= './device-map.yml'
-
     try:
         dm = request.param
-    except:
-        dm= './device-map.yml'
+    except AttributeError:
+        dm = './device-map.yml'
     logs = './logs'
     devices = device.load_devices(dm)
     assert len(devices) > 0
     with logfile(logs, __name__) as f:
-        shellb = shell_open(devices[0]['shell'], log_to=f)
-        yield (shellb)
-        shellb.close()
+        this_shell = shell_open(devices[0]['shell'][0], log_to=f)
+        yield this_shell
+        this_shell.close()
+
 
 @pytest.fixture(scope='function')
 def reset(connected_device):
     (shell) = connected_device
     shell.pyocd_reset()
     yield
-
